5890b733126eb7b7f5feab27259c14f7b61b01a0
J-SLY <2029872426@qq.com>
2026-09-26 19:16:31 +0800

    Feat：丰富了测试

--- test/main.test.ts
+++ test/main.test.ts
@@ -5,24 +5,33 @@ import pluginRegister from '../src/core/plugin/register' // 按你实际导出�
 type Meta = { type: string; handler: any }
 
 function fake(entries: Array<[string, Meta]> = []) {
-  return {
-    handers: new Map<string, Meta>(entries),
-    client: {
-      command: vi.fn(),
-      onRequest: vi.fn(),
-      use: vi.fn(),
-    },
-  } as unknown as JsBot
+    return {
+        handers: new Map<string, Meta>(entries),
+        client: {
+            command: vi.fn(),
+            onRequest: vi.fn(),
+            use: vi.fn(),
+        },
+    } as unknown as JsBot
 }
 
 describe('Register Plugin', () => {
-  it('Command 类型调用 client.command', () => {
-    const handler = vi.fn()
-    const bot = fake([['echo', { type: 'Command', handler }]])
+    it('Command 类型调用 client.command', () => {
+        const handler = vi.fn()
+        const plugins: Array<[string, Meta]> = [
+            ['echo', { type: 'Command', handler }],
+            ['info', { type: 'Command', handler }],
+            ['test', { type: 'Middleware', handler }]
+        ]
 
-    pluginRegister(bot)                          // ← 调用被测函数
+        const bot = fake(plugins)
 
-    expect(bot.client.command).toHaveBeenCalledTimes(1)      // ← 断言
-    expect(bot.client.command).toHaveBeenCalledWith('echo', handler)
-  })
+        pluginRegister(bot)
+        for(const plugin of plugins){
+            if(plugin[1].type==='Command')
+                expect(bot.client.command).toHaveBeenCalledWith(plugin[0], plugin[1].handler)
+            if(plugin[1].type==='Middleware')
+                expect(bot.client.use).toHaveBeenCalledWith(plugin[1].handler)
+        }
+    })
 })
